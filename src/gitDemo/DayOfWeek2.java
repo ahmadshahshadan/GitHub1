ad1348aaf7ee79cea39a5420e6e803e41c6a8756
ahmadshahsha <110962479+ahmadshahsha@users.noreply.github.com>
2023-01-29 01:09:30 -0500

    Hi Please see a check it

---
+++ src/gitDemo/DayOfWeek2.java
@@ -0,0 +1,30 @@
+package gitDemo;
+
+public class DayOfWeek2 {
+	
+	public static void main(String[] args) {
+		
+		months();
+	}
+	public static void months() {
+		 
+	
+		int months = 4;
+		switch (months) {
+	case 1:
+		System.out.println("January");
+		break;
+	case 2:
+		System.out.println("February");
+		break;
+	case 3:
+		System.out.println("March"); 
+		break;
+		default:
+			System.out.println("invalid number");
+		}
+		
+		System.out.println("Good Job ");
+		System.out.println("998877777");
+	}
+}
