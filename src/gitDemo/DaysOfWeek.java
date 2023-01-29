03417139061df4c5a4f1e8610891ba5162d97b9f
ahmadshahshadan <shadan300@gmail.com>
2023-01-28 23:53:37 -0600

    GitHub1 for Testing Team

---
+++ src/gitDemo/DaysOfWeek.java
@@ -0,0 +1,28 @@
+package gitDemo;
+
+public class DaysOfWeek {
+	public static void main(String[] args) {
+		
+		months();
+	}
+	public static void months() {
+		 
+	
+		int months = 4;
+		switch (months) {
+	case 1:
+		System.out.println("January");
+		break;
+	case 2:
+		System.out.println("February");
+		break;
+	case 3:
+		System.out.println("March"); 
+		break;
+		default:
+			System.out.println("invalid number");
+		}
+	}
+}
+
+
